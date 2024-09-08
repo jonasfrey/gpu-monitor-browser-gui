0e07dff3d708bd3dfacb6c7dcf999173099882bd
Your Name <you@example.com>
2024-09-08 23:56:14 +0200

    asdf

--- localhost/tmp/TimeChart-1.0.0-beta.10/src/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-import core from '../core';
-import { TimeChartPlugins } from '../options';
-
-export interface TimeChartPlugin<TState=any> {
-    apply(chart: core<TimeChartPlugins>): TState;
-}
--- localhost/tmp/TimeChart-1.0.0-beta.10/src/plugins_extra/index.ts
+++
@@ -1,2 +0,0 @@
-export { EventsPlugin } from './events';
-export { SelectZoomPlugin } from './selectZoom';
